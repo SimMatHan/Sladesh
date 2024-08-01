35b396e19b0daaeee3ba39c18e79afbcf2d081e9
Simon Mathias Hansen <simonmathiashansen@gmail.com>
2024-08-01 13:49:29 +0200

    RequestForm updated with message if there is no sladesh

--- src/components/RequestForm.js
+++ src/components/RequestForm.js
@@ -70,14 +70,18 @@ const RequestForm = ({ user }) => {
         <button type="submit" className="form-button">Send Sladesh</button>
       </form>
       <div className="request-list-container">
-        <h2>Requests</h2>
-        <ul className="request-list">
-          {requests.map((request, index) => (
-            <li key={index} className="request-item">
-              {request.message}
-            </li>
-          ))}
-        </ul>
+        <h2>Any Sladesh for you?!</h2>
+        {requests.length === 0 ? (
+          <p className="no-requests-message">You're safe! No Sladesh for you at the moment.</p>
+        ) : (
+          <ul className="request-list">
+            {requests.map((request, index) => (
+              <li key={index} className="request-item">
+                {request.message}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
