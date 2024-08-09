d53cc375997b251fe04423c7b39a71d3d0fbc60a
Simon Mathias Hansen <simonmathiashansen@gmail.com>
2024-08-09 16:43:56 +0200

    Header and other css has been improved

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,61 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Header.css';
-import CallMeEmoji from '../assets/CallMeEmoji.svg';
-import ClinkingBeerMugs from '../assets/ClinkingBeerMugs.svg';
-import ScoreboardIcon from '../assets/Scoreboard.svg';
-import OpenMailbox from '../assets/OpenMailbox.svg'; // Import the new icon
+import Sladesh from '../assets/headersvgs/RocketLaunchOutlined.svg';
+import Drink from '../assets/headersvgs/LocalDrinkOutlined.svg';
+import Leaderboard from '../assets/headersvgs/LeaderboardOutlined.svg';
+import SladeshHub from '../assets/headersvgs/MarkunreadMailboxOutlined.svg';
+import Chart from '../assets/headersvgs/AreaChartOutlined.svg';
+import SladeshFilled from '../assets/headersvgs/RocketLaunchFilled.svg';
+import DrinkFilled from '../assets/headersvgs/LocalDrinkFilled.svg';
+import LeaderboardFilled from '../assets/headersvgs/LeaderboardFilled.svg';
+import SladeshHubFilled from '../assets/headersvgs/MarkunreadMailboxFilled.svg';
+import ChartFilled from '../assets/headersvgs/AreaChartFilled.svg';
 
 const Header = ({ sladeshCount }) => {
+  const location = useLocation();
+
   return (
     <header className="header-container">
       <div className="nav-container">
         <div className="nav-links">
           <Link to="/">
-            <img src={ClinkingBeerMugs} alt="Drinks Ohøj" className="nav-icon" />
+            <img
+              src={location.pathname === '/' ? DrinkFilled : Drink}
+              alt="Drinks"
+              className="nav-icon"
+            />
           </Link>
           <Link to="/requests">
-            <img src={CallMeEmoji} alt="Sladesh" className="nav-icon" />
-          </Link>
-          <Link to="/scoreboard">
-            <img src={ScoreboardIcon} alt="Scoreboard" className="nav-icon" />
+            <img
+              src={location.pathname === '/requests' ? SladeshFilled : Sladesh}
+              alt="Sladesh"
+              className="nav-icon"
+            />
           </Link>
           <Link to="/sladesh-hub">
             <div className="nav-icon-container">
-              <img src={OpenMailbox} alt="Sladesh Hub" className="nav-icon" />
+              <img
+                src={location.pathname === '/sladesh-hub' ? SladeshHubFilled : SladeshHub}
+                alt="Sladesh Hub"
+                className="nav-icon"
+              />
               {sladeshCount > 0 && <div className="notification-circle">{sladeshCount}</div>}
             </div>
+          </Link>
+          <Link to="/scoreboard">
+            <img
+              src={location.pathname === '/scoreboard' ? LeaderboardFilled : Leaderboard}
+              alt="Scoreboard"
+              className="nav-icon"
+            />
+          </Link>
+          <Link to="/chart">
+            <img
+              src={location.pathname === '/chart' ? ChartFilled : Chart}
+              alt="Chart"
+              className="nav-icon"
+            />
           </Link>
         </div>
       </div>
